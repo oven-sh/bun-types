0db75326cef43def68974cc77d9976bc2687e4e4
xHyroM <generalkubo@gmail.com>
2022-07-28 11:31:22 +0200

    feat: some tests

--- tests/index.test-d.ts
+++ tests/index.test-d.ts
@@ -1,5 +1,5 @@
 import { expectType } from "tsd";
-import Bun from ".";
+import Bun, { fs, fsPromises } from ".";
 
 // Testing ../bun.d.ts
 
@@ -12,7 +12,7 @@ expectType<Promise<string>>(Bun.file("index.test-d.ts").text());
 expectType<number>(Bun.file("index.test-d.ts").size);
 expectType<string>(Bun.file("index.test-d.ts").type);
 
-// Hash
+// - Hash 
 expectType<string>(new Bun.MD4().update("test").digest("hex"));
 expectType<string>(new Bun.MD5().update("test").digest("hex"));
 expectType<string>(new Bun.SHA1().update("test").digest("hex"));
@@ -28,3 +28,14 @@ expectType<URL>(Bun.pathToFileURL("/foo/bar.txt"));
 expectType<string>(Bun.fileURLToPath(new URL("file:///foo/bar.txt")));
 
 // Testing ../fs.d.ts
+expectType<string>(fs.readFileSync('./index.d.ts', { encoding: 'utf-8' }).toString());
+expectType<boolean>(fs.existsSync('./index.d.ts'));
+expectType<void>(fs.accessSync('./index.d.ts'));
+expectType<void>(fs.appendFileSync('./index.d.ts', 'test'));
+expectType<void>(fs.mkdirSync('./index.d.ts'));
+
+// Testing ^promises.d.ts
+expectType<string>((await fsPromises.readFile('./index.d.ts', { encoding: 'utf-8' })).toString());
+expectType<Promise<void>>(fsPromises.access('./index.d.ts'));
+expectType<Promise<void>>(fsPromises.appendFile('./index.d.ts', 'test'));
+expectType<Promise<void>>(fsPromises.mkdir('./index.d.ts'));
