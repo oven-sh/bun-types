745af50341f3f42997db8f014c17c65d9e26886f
jhmaster2000 <32803471+jhmaster2000@users.noreply.github.com>
2022-07-30 22:48:03 -0300

    Run prettier on fs/promises for the 1st time

--- fs.d.ts
+++ fs.d.ts
@@ -3,17 +3,17 @@
  * way modeled on standard POSIX functions.
  *
  * To use the promise-based APIs:
- * 
+ *
  * ```js
  * import * as fs from 'fs/promises';
  * ```
  *
  * To use the callback and sync APIs:
- * 
+ *
  * ```js
  * import * as fs from 'fs';
  * ```
- * 
+ *
  * All file system operations have synchronous and callback
  * forms, and are accessible using both CommonJS syntax and ES6 Modules (ESM).
  */
